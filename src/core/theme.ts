47a8edda5691035a23e18aa7e6832bec1231ca4b
CoralieFouque <80695879+CoralieFouque@users.noreply.github.com>
2022-04-27 10:36:37 +0200

    ✨(component) : add button component

--- src/core/theme.ts
+++ src/core/theme.ts
@@ -11,13 +11,17 @@ const Theme = {
         primary : '#E3634B', // orange
         secondary : '#1C393A', //green
         light: '#D4D6CB',
-        border_color: '#CDD4D9', //light grey
         white: '#FFFFFF',
         black: '#000000',
         error: 'red',
+        border_color: '#CDD4D9', //light grey
+        button_text_color: '#FFFFFF',
+        font_color: '#000000',
 
         input_border_radius: 8,
+        button_border_radius: 15,
         text_size: 16,
+        button_text_size: 20,
     },
 }; 
 
