bafc1c9fd2d5ec9af744d105e3eb5acfa68ed9bd
CoralieFouque <80695879+CoralieFouque@users.noreply.github.com>
2022-07-13 17:56:54 +0200

    ♻️ (home) : change home

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,30 +1,51 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React, { FunctionComponent } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { View } from 'react-native';
 import Text from '../components/atoms/text.component';
 import Button from '../components/molecules/button.component';
+import AnimalCard from '../components/organisms/animal-card.component';
 import HomeTemplate from '../components/templates/home.template';
 import Theme from '../core/theme';
 import { RootStackParamList } from '../root-stack-param-list';
+import Animal from '../core/types/animal.type';
 
 type Props = {
   navigation : NativeStackNavigationProp<RootStackParamList, 'Home'>;
 }
 
 const Home : FunctionComponent<Props> = (props: Props) => {
+  const [animal, setAnimal] = useState<Animal>({});
+
+  function fetchOne() : void {
+    fetch('http://192.168.255.12:3001/animal/sos')
+      .then((res) => res.json())
+      .then((data) => setAnimal(data))
+  }
+
+  useEffect(() => {
+    fetchOne();
+  }, []);
+  
+
   return(
     <HomeTemplate
       footer
       header
-      rightIcon='menu'
     >
-      <View style={{ alignItems:'center', justifyContent:'space-around', flex:1, width:'100%' }}>
+      <View style={{ alignItems:'center', justifyContent:'space-evenly', flex:1, width:'100%', paddingBottom: 10 }}>
         <Text color={Theme.default.brown} font={Theme.default.font_poppins_semibold} size={30}>A la Une</Text>
-        <Text size={25} >Ils ont besoin de toi ...</Text>
-        <View style={{ backgroundColor:'lightgrey', height:356, width:256, borderRadius:15 }}/>
-        <View style={{ backgroundColor:'grey', height:13, width:100, borderRadius:15 }}/>
+        <Text size={25}>Et pourquoi pas eux ?</Text>
+          <AnimalCard
+            style = {{ justifyContent:'flex-start' }}
+            animal = {animal}
+            isSos
+            height={356}
+            width={256}
+          />
         <Button style={{ width:205 }} onPress={() => props.navigation.navigate('Match') }>Commencer</Button>
-      </View>
+        </View>
     </HomeTemplate>
     
   );
